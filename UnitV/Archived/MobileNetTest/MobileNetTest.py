7eaadc7dbbb3e2e0edeea1c5e5d94ebdac84bba6
shirokuma89dev <code@shirokuma89.dev>
2024-02-06 00:11:22 +0900

    ⚡️ とりあえずHSU

--- UnitV/Archived/MobileNetTest/MobileNetTest.py
+++ UnitV/Archived/MobileNetTest/MobileNetTest.py
@@ -7,18 +7,22 @@ import KPU as kpu
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
-sensor.set_vflip(1)
-sensor.set_hmirror(1)
-sensor.run(1)
-labels = ['0', '1']
+sensor.set_windowing((0, 0, 224, 224)
+sensor.run()
+
+labels = ['H', 'S', 'U']
+BLACK = (0, 22, -32, 26, -34, 26)
 print(labels)
 task = kpu.load("/sd/m.kmodel")
+
 while(True):
     img = sensor.snapshot()
+    img.binary([BLACK])
 
     # モデル入力用の画像サイズに変換
     img2 = img.resize(224, 224)
     # K210のRGB565 データを推論処理用のメモリブロック ​​RGB888 にコピー
+
     img2.pix_to_ai()
     # モデルの実行
     fmap = kpu.forward(task, img2)
@@ -30,9 +34,7 @@ while(True):
 #   Avoiding Exception for index of undef result.
     if pmax >=0 and pmax <=1:
         max_index = plist.index(pmax)
-        img.draw_string(2, 10, "%s " % (labels[max_index].strip()), 20)
         print("DETECT:" + labels[max_index].strip())
-        print(plist.index(pmax))
     else:
         print("NOT DETECT")
 a = kpu.deinit(task)
