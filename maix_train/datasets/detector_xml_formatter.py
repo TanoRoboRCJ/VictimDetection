34e7376fad9bf53d201cb94ef9faeb0ea3c3d699
shirokuma89dev <code@shirokuma89.dev>
2024-02-08 00:58:24 +0900

    ✨ ラベルを取得

--- maix_train/datasets/detector_xml_formatter.py
+++ maix_train/datasets/detector_xml_formatter.py
@@ -2,10 +2,15 @@ ORIGIN_DIR_PATH = './DAMMY'
 DEST_DIR_PATH = './DAMMY_FORMATTED'
 
 import os
+import shutil
+import xml.etree.ElementTree as ET
+
+xml_names = []
+labels = []
 
 def create_dest_dir():
     if os.path.exists(DEST_DIR_PATH):
-        print('Directory {DEST_DIR_PATH} already exists. Data will be deleted.')
+        print('Directory ' + DEST_DIR_PATH + ' already exists. Data will be deleted.')
         should_continue = input('Do you want to continue? [yes/no]: ')
 
         while should_continue.lower() not in ['yes', 'no']:
@@ -16,14 +21,36 @@ def create_dest_dir():
             exit()
         
         print('Deleting directory...')
-        os.rmdir(DEST_DIR_PATH)
+        shutil.rmtree(DEST_DIR_PATH)
     
+    print('Creating directory... ' + DEST_DIR_PATH)
     os.makedirs(DEST_DIR_PATH)
-        
+    os.makedirs(DEST_DIR_PATH + '/images')
+    os.makedirs(DEST_DIR_PATH + '/xml')
 
+def get_xml_name_list():
+    print('Getting xml names...')
+
+    global xml_names
+    xml_names = [
+        xml_name for xml_name in os.listdir(ORIGIN_DIR_PATH + '/annotations') if os.path.isfile(os.path.join(ORIGIN_DIR_PATH + '/annotations', xml_name))
+    ]
+
+def copy_xml_files(xml_name):
+    xml_content = ET.parse(ORIGIN_DIR_PATH + '/annotations/' + xml_name)
+
+    label = xml_content.find(".//name")
+    print(label.text)
 
 def __main__():
     create_dest_dir()
+    get_xml_name_list()
+
+    for xml_name in xml_names:
+        copy_xml_files(xml_name)
+        print('Copying xml file...' + xml_name)
+
+
     print('Done')
 
 __main__()
