2e54642703467de9e3edde17f2b3e4065c4b9315
shirokuma89dev <code@shirokuma89.dev>
2024-03-10 21:59:21 +0900

    ♻️

--- maix_train/train/config_template.py
+++ maix_train/train/config_template.py
@@ -1,36 +1,31 @@
+# Mobilenet（分類器）
+classifier_train_epochs = 40  # 分類器の訓練エポック数
+classifier_train_batch_size = 5  # 分類器の訓練バッチサイズ
+classifier_train_max_classes_num = 15  # 分類器の訓練で扱う最大クラス数
+classifier_train_one_class_min_img_num = 40  # クラスに必要な最小画像数
 
+# Yolo（物体認識）
+detector_train_epochs = 40  # Yoloの訓練エポック数
+detector_train_batch_size = 5  # Yoloの訓練バッチサイズ（下げると学習が遅いが精度高）
+detector_train_learn_rate = 1e-4  # Yoloの訓練学習率
+detector_train_max_classes_num = 15  # Yoloの訓練で扱う最大クラス数
+detector_train_one_class_min_img_num = 100  # Yoloの訓練で一つのクラスに必要な最小画像数
 
+## 以下はいじるな！！
+classifier_train_gpu_mem_require = 2 * 1024 * 1024 * 1024
+detector_train_gpu_mem_require = 2 * 1024 * 1024 * 1024
 
-import os
-curr_dir = os.path.abspath(os.path.dirname(__file__))
-
-
-# kmodel convert
-                 # "/ncc/ncc"  # download from https://github.com/kendryte/nncase/releases/tag/v0.1.0-rc5
-ncc_kmodel_v3 =  os.path.join(curr_dir, "..", "tools", "ncc", "ncc_v0.1/ncc")  
-sample_image_num = 20       # convert kmodel sample image (for quantizing)
-
-# train
-allow_cpu = True # True
-
-# classifier
-classifier_train_gpu_mem_require = 2*1024*1024*1024
-classifier_train_epochs = 40
-classifier_train_batch_size = 5
-classifier_train_max_classes_num = 15
-classifier_train_one_class_min_img_num = 40            # 一个类别中至少需要的样本数量
-classifier_train_one_class_max_img_num = 2000          # 一个类别中最多需要的样本数量
 classifier_result_file_name_prefix = "maixhub_classifier_result"
-
-# detector
-detector_train_gpu_mem_require = 2*1024*1024*1024
-detector_train_epochs = 40
-detector_train_batch_size = 5
-detector_train_learn_rate = 1e-4
-detector_train_max_classes_num = 15         # 最多能训练多少类
-detector_train_one_class_min_img_num = 100            # 一个类别中至少需要的样本数量
-detector_train_one_class_max_img_num = 2000           # 一个类别中最多需要的样本数量
 detector_result_file_name_prefix = "maixhub_detector_result"
 
+classifier_train_one_class_max_img_num = 5000
+detector_train_one_class_max_img_num = 5000
 
+import os
 
+curr_dir = os.path.abspath(os.path.dirname(__file__))
+
+ncc_kmodel_v3 = os.path.join(curr_dir, "..", "tools", "ncc", "ncc_v0.1/ncc")
+sample_image_num = 20  # convert kmodel sample image (for quantizing)
+
+allow_cpu = True
